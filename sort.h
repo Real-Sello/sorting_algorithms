415ec587d331cc03ca6009d8db80b3c9234e8c84
Sello Prince Moneatse <spmoneatse@gmail.com>
2023-02-20 23:41:32 +0200

    Update on header

--- sort.h
+++ sort.h
@@ -1,5 +1,5 @@
-#ifndef SORT_H
-#define SORT_H
+#ifndef __SORT_H__
+#define __SORT_H__
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -19,14 +19,26 @@ typedef struct listint_s
 } listint_t;
 
 /*functions*/
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+/*sorting functions*/
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void print_list(const listint_t *list);
-void print_array(const int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+int partition(int *array, int lo, int hi, size_t size);
+void quick_s(int *array, int lo, int hi, size_t size);
+void merge(size_t lo, size_t mi, size_t hi, int *dest, int *src);
+void merge_partition(size_t lo, size_t hi, int *array, int *base);
+void swap_root(int *array, size_t root, size_t hi, size_t size);
 
-/*Aux functions*/
-int partition(int *array, int low, int high,  size_t size);
-void recursive_sort(int *array, int low, int high, size_t size);
-#endif /*SORT*/
+#endif /*__SORT__*/
